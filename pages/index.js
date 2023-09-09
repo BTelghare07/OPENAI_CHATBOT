4a07fc7c19a5d4bf6647489207112cb275d833cf
Bhagyashree Vijay Telghare <114392423+BTelghare07@users.noreply.github.com>
2023-09-09 13:02:23 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,270 @@
+import React from "react";
+import Head from "next/head";
+
+import { getMenu, getGDPRBanner } from "../lib/api";
+
+import Layout from "../components/layouts/globalTemplate";
+import Footer from "../components/navigation/footer";
+
+import SideBar from "../components/navigation/sideBar";
+import ArrowLinkContainer from "../components/navigation/arrowLinkContainer";
+import ArrowLink from "../components/navigation/arrowLink";
+
+import GDPRBanner from "../components/utilities/gdpr";
+import SocialCallouts from "../components/utilities/socialCallout";
+import Spacer from "../components/utilities/spacer";
+
+import SummaryTiles from "../components/summaryTiles";
+
+import NewsContainer from "../components/layouts/newsContainer";
+import InlineCalloutContainer from "../components/layouts/inlineCalloutContainer";
+import TileContainer from "../components/layouts/tileContainer";
+import RefCard from "../components/blocks/refCard";
+
+import { H1, H2 } from "../components/blocks/headers";
+import NewsFeed from "../components/blocks/newsFeed";
+import Button from "../components/blocks/button";
+import InlineCallout from "../components/blocks/inlineCallout";
+import NoteSplit from "../components/blocks/noteSplit";
+
+import { attributes } from "../content/index.md";
+
+import styles from "../components/layouts/container.module.css";
+
+export default function Home({ window, menu, gdpr_data }) {
+  let { description } = attributes;
+
+  return (
+    <Layout window={window}>
+      <Head>
+        <title>Streamlit documentation</title>
+        <link rel="icon" href="/favicon.svg" />
+        <link rel="alternate icon" href="/favicon32.ico" />
+        <meta name="theme-color" content="#ffffff" />
+        <link
+          rel="canonical"
+          href={`https://${process.env.NEXT_PUBLIC_HOSTNAME}`}
+        />
+        <meta content="Streamlit Docs" property="og:title" />
+        <meta content="Streamlit Docs" name="twitter:title" />
+        {description && (
+          <React.Fragment>
+            <meta content={description} name="description" />
+            <meta content={description} property="og:description" />
+            <meta content={description} name="twitter:description" />
+          </React.Fragment>
+        )}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://docs.streamlit.io/" />
+        <meta content="summary_large_image" name="twitter:card" />
+        <meta
+          property="og:image"
+          content={`https://${process.env.NEXT_PUBLIC_HOSTNAME}/sharing-image-facebook.jpg`}
+        />
+        <meta
+          name="twitter:image"
+          content={`https://${process.env.NEXT_PUBLIC_HOSTNAME}/sharing-image-twitter.jpg`}
+        />
+      </Head>
+      <GDPRBanner {...gdpr_data} />
+      <section className={styles.Container}>
+        <SideBar menu={menu} slug={[]} />
+        <section className={styles.InnerContainer}>
+          <article>
+            <H1>Streamlit documentation</H1>
+            <p>
+              <a href="https://www.streamlit.io">Streamlit</a> is an open-source
+              Python library that makes it easy to create and share beautiful,
+              custom web apps for machine learning and data science. In just a
+              few minutes you can build and deploy powerful data apps. So let's
+              get started!
+            </p>
+
+            <Spacer size="2rem" />
+
+            <SummaryTiles />
+
+            {/* <H2 className='no-b-m'>What's new</H2>
+
+            <TileContainer>
+              <Tile size="half" background="unset" color="unset" dark={{ background: "unset", color: 'white', border_color: 'gray-90' }} border_color="gray-40" img="/logo.svg" title="Feature title" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend a facilisis sagittis, vitae nibh massa in facilisis et. Pretium eget non cursus purus tempus porta sodales." link="/tutorials/get-started" />
+              <Tile size="half" background="unset" color="unset" dark={{ background: "unset", color: 'white', border_color: 'gray-90' }} border_color="gray-40" icon="visibility" title="Feature title" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend a facilisis sagittis, vitae nibh massa in facilisis et. Pretium eget non cursus purus tempus porta sodales." link="/tutorials/get-started" />
+              <Tile size="half" background="unset" color="unset" dark={{ background: "unset", color: 'white', border_color: 'gray-90' }} border_color="gray-40" icon="edit" title="Feature title" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend a facilisis sagittis, vitae nibh massa in facilisis et. Pretium eget non cursus purus tempus porta sodales." link="/tutorials/get-started" />
+              <Tile size="half" background="unset" color="unset" dark={{ background: "unset", color: 'white', border_color: 'gray-90' }} border_color="gray-40" img="/logo.svg" title="Feature title" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend a facilisis sagittis, vitae nibh massa in facilisis et. Pretium eget non cursus purus tempus porta sodales." link="/tutorials/get-started" />
+            </TileContainer> */}
+
+            <H2>How to use our docs</H2>
+            <InlineCalloutContainer>
+              <InlineCallout
+                color="violet-70"
+                icon="description"
+                bold="Streamlit library"
+                href="/library/get-started"
+              >
+                includes our Get started guide, API reference, and more advanced
+                features of the core library including caching, theming, and
+                Streamlit Components.
+              </InlineCallout>
+              <InlineCallout
+                color="l-blue-70"
+                icon="cloud"
+                bold="Streamlit Community Cloud"
+                href="/streamlit-community-cloud"
+              >
+                is an open and free platform for the community to deploy,
+                discover, and share Streamlit apps and code with each other.
+                Create a new app, share it with the community, get feedback,
+                iterate quickly with live code updates, and have an impact!
+              </InlineCallout>
+              <InlineCallout
+                color="orange-70"
+                icon="school"
+                bold="Knowledge base"
+                href="/knowledge-base"
+              >
+                is a self-serve library of tips, step-by-step tutorials, and
+                articles that answer your questions about creating and deploying
+                Streamlit apps.
+              </InlineCallout>
+              {/* <InlineCallout color="green-70" icon="code" bold="Cookbook" href="/cookbook">
+                provides short code snippets that you can copy in for specific use cases.
+              </InlineCallout>
+              <InlineCallout color="red-70" icon="question_answer" bold="Support" href="/support">
+                are the bread and butter of how our APIs and configuration files work and will give you short, actionable explanations of specific functions and features.
+              </InlineCallout> */}
+            </InlineCalloutContainer>
+
+            <H2 className="no-b-m">What's new</H2>
+
+            <TileContainer>
+              <RefCard
+                size="third"
+                href="/library/api-reference/status/st.status"
+              >
+                <i className="material-icons-sharp">rotate_right</i>
+                <h4>Status container</h4>
+                <p>
+                  Introducing <code>st.status</code> to display output from
+                  long-running processes and external API calls
+                </p>
+              </RefCard>
+              <RefCard
+                size="third"
+                href="/library/api-reference/widgets/st.toggle"
+              >
+                <i className="material-icons-sharp">toggle_on</i>
+                <h4>Toggle switch</h4>
+                <p>
+                  Introducing <code>st.toggle</code> — an alternative to
+                  <code>st.checkbox</code> when you need an on/off switch.
+                </p>
+              </RefCard>
+              <RefCard size="third" href="/library/api-reference/charts">
+                <i className="material-icons-sharp">palette</i>
+                <h4>Color parameter for simple charts</h4>
+                <p>
+                  Set the series color in <code>st.line_chart</code>,
+                  <code>st.bar_chart</code>, and <code>st.area_chart</code>
+                  with a new color parameter.
+                </p>
+              </RefCard>
+              <RefCard
+                size="third"
+                href="/library/advanced-features/app-menu#deploy-this-app"
+              >
+                <i className="material-icons-sharp">rocket</i>
+                <h4>Local to Community Cloud just got easier</h4>
+                <p>
+                  Locally running apps now have a Deploy button in their
+                  toolbars.
+                </p>
+              </RefCard>
+              <RefCard
+                size="third"
+                href="/library/api-reference/text/st.markdown"
+              >
+                <i className="material-icons-sharp">looks</i>
+                <h4>More colors in markdown</h4>
+                <p>
+                  Enjoy more ways to color your text with gray/grey and rainbow
+                  options wherever color in markdown is supported.
+                </p>
+              </RefCard>
+              <RefCard
+                size="third"
+                href="/library/api-reference/text/st.header"
+              >
+                <i className="material-icons-sharp">horizontal_rule</i>
+                <h4>Colored header dividers</h4>
+                <p>
+                  Add colored dividers to <code>st.header</code> and
+                  <code>st.subheader</code>. Dividers can be blue, green,
+                  orange, red, violet, gray/grey, or rainbow!
+                </p>
+              </RefCard>
+              {/* <Tile
+                size="half"
+                background="unset"
+                color="unset"
+                dark={{
+                  background: "unset",
+                  color: "white",
+                  border_color: "gray-90",
+                }}
+                border_color="gray-40"
+                img="/logo.svg"
+                title="Clear memo + singleton caches procedurally"
+                text="Do you need more control over cache invalidation? Any function annotated with @st.cache_data or @st.cache_resource gets its own clear() function automatically."
+                link="/library/advanced-features/experimental-cache-primitives#clear-memo-and-singleton-caches-procedurally"
+              /> */}
+              {/* Preserve above comment for syntax reference */}
+            </TileContainer>
+
+            <H2 className="no-b-m">Latest blog posts</H2>
+
+            <NewsContainer>
+              <NewsFeed />
+
+              <Button link="https://blog.streamlit.io/" target="_blank">
+                View all updates
+              </Button>
+            </NewsContainer>
+
+            <NoteSplit
+              background="gray-20"
+              title="Join the community"
+              copy="Streamlit is more than just a way to make data apps, it's also a community of creators that share their apps and ideas and help each other make their work better. Please come join us on the community forum. We love to hear your questions, ideas, and help you work through your bugs — stop by today!"
+              button={{
+                text: "View forum",
+                link: "https://discuss.streamlit.io",
+                target: "_blank",
+              }}
+              image="/join.png"
+            />
+          </article>
+
+          <SocialCallouts />
+
+          <ArrowLinkContainer>
+            <ArrowLink
+              link="/library/get-started"
+              type="next"
+              content="Get started"
+            />
+          </ArrowLinkContainer>
+        </section>
+        <Footer />
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps(context) {
+  const props = {};
+  props["menu"] = getMenu();
+  props["gdpr_data"] = await getGDPRBanner();
+
+  return {
+    props: props,
+  };
+}
